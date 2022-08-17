65b73b1feb11be7641401b6e3a6018915603b213
Romanojp <jp_romana@outlook.com>
2022-08-17 00:29:58 -0300

    Criando a Classe Category

--- Backend/dscatalog/src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
+++ Backend/dscatalog/src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
@@ -1,4 +1,4 @@
-package com.example.algamoney.api;
+package com.devsuperior.dscatalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
