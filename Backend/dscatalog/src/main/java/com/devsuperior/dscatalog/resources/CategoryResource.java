1b93f867e9373fdc13a1dd5914a33f9eef01a1c6
Romanojp <jp_romana@outlook.com>
2022-08-19 01:07:18 -0300

    Implementando e configurando a classe DTO

--- Backend/dscatalog/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ Backend/dscatalog/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -2,12 +2,13 @@ package com.devsuperior.dscatalog.resources;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.services.CategoryService;
 
 
@@ -16,11 +17,12 @@ import com.devsuperior.dscatalog.services.CategoryService;
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 	
+	@Autowired
 	private CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = service.findAll();
+	public ResponseEntity<List<CategoryDTO>> findAll(){
+		List<CategoryDTO> list = service.findAll();
 		
 		return ResponseEntity.ok().body(list);
 	}
