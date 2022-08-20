ddeaca78e9c607065b0ea88666b553767b363e21
Romanojp <jp_romana@outlook.com>
2022-08-20 12:05:48 -0300

    buscando categoria por id

--- Backend/dscatalog/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ Backend/dscatalog/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +26,13 @@ public class CategoryResource {
 		List<CategoryDTO> list = service.findAll();
 		
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+		CategoryDTO dto = service.findById(id);
+		
+		return ResponseEntity.ok().body(dto);
 	}
 
 }
--- Backend/dscatalog/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ Backend/dscatalog/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,14 @@ public class CategoryService {
 
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 
+	}
+	
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.get();
+		
+		return new CategoryDTO(entity);
 	}
 
 }
