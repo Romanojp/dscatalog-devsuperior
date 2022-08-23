bfe1cc6fbf4fcf99c14d80c4de566e6ff97bc009
Romanojp <jp_romana@outlook.com>
2022-08-23 00:03:25 -0300

    01-23 Entidade Product, revisão modelo relacional N-N 01-24 Mapeamento JPA N-N, script SQL completo

--- Backend/dscatalog/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ Backend/dscatalog/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -1,8 +1,10 @@
 package com.devsuperior.dscatalog.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -21,6 +23,13 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	
+	@Column(columnDefinition = "TIMESTAMP WITH TIME ZONE")
+	private Instant createdAt;
+	
+	@Column(columnDefinition = "TIMESTAMP WITH TIME ZONE")
+	private Instant updatedAT;
 
 	public Category() {
 	}
