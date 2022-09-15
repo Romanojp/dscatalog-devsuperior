e6d905612b0e3711553747ff1318c71cf7c10834
Romanojp <jp_romana@outlook.com>
2022-09-14 23:26:48 -0300

    03-06 CRUD de usuários PARTE 1

---
+++ Backend/dscatalog/src/main/java/com/devsuperior/dscatalog/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dscatalog.entities.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
+
+}
---
+++ Backend/dscatalog/src/main/java/com/devsuperior/dscatalog/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dscatalog.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
