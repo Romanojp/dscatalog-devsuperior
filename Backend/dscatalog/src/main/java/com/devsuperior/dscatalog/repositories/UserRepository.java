51056337752c781e395b46bfa14c9a2f64a1b3e0
Romanojp <jp_romana@outlook.com>
2022-09-20 23:26:08 -0300

    03-13 Implementando um ConstraintValidator customizado

--- Backend/dscatalog/src/main/java/com/devsuperior/dscatalog/repositories/UserRepository.java
+++ Backend/dscatalog/src/main/java/com/devsuperior/dscatalog/repositories/UserRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.devsuperior.dscatalog.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
+	
+	User findByEmail(String email);
 
 }
