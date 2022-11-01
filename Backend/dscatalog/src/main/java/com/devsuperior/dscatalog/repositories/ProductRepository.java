56b360de2c209038313e37f04218269f9a670fc1
Romanojp <jp_romana@outlook.com>
2022-11-01 00:07:36 -0300

    05-29 Filtrar produtos, COALESCE, List

--- Backend/dscatalog/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
+++ Backend/dscatalog/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dscatalog.repositories;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,8 +16,8 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 	
 	
 	@Query("SELECT DISTINCT obj FROM Product obj INNER JOIN  obj.categories cats "
-			+ "WHERE (:category IS NULL OR :category IN cats) AND "
-			+ "(LOWER(obj.name) LIKE LOWER(CONCAT ('%',:name, '%')) )")
-	Page<Product> find(Category category,String name, Pageable pageable);
+			+ "WHERE (COALESCE(:category) IS NULL OR :category IN cats) AND "
+			+ "(:name = '' OR LOWER(obj.name) LIKE LOWER (CONCAT('%', :name,'%')))")
+	Page<Product> find(List<Category> category,String name, Pageable pageable);
 
 }
--- Backend/dscatalog/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ Backend/dscatalog/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dscatalog.services;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
@@ -33,9 +35,9 @@ public class ProductService {
 	@Transactional(readOnly = true)
 	public Page<ProductDTO> findAllPaged(Long categoryId, String name, Pageable pageable) {
 		
-		Category category =(categoryId ==0) ? null : categoryRepository.getOne(categoryId);
+		List<Category> categories =(categoryId ==0) ? null : Arrays.asList(categoryRepository.getOne(categoryId));
 		
-		Page<Product> list = repository.find(category, name, pageable);
+		Page<Product> list = repository.find(categories, name, pageable);
 		return list.map(x -> new ProductDTO(x));
 	}
 
